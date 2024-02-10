cb7116d62c308295f2f69b861bb3bc94bdbbd2b3
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2024-02-10 00:47:23 -0500

    final version

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { MantineProvider } from '@mantine/core'
 import '../css/index.css'
 import React from 'react'
 import Home from './Home'
@@ -10,13 +9,11 @@ import Dashboard from './Dashboard'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
-		<MantineProvider>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/login' element={<Login />} />
-				<Route path='/register' element={<Register />} />
-				<Route path='/dashboard' element={<Dashboard />} />
-			</Routes>
-		</MantineProvider>
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/login' element={<Login />} />
+			<Route path='/register' element={<Register />} />
+			<Route path='/dashboard' element={<Dashboard />} />
+		</Routes>
 	</BrowserRouter>
 )
